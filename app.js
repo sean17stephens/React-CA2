4d1d0b87d696a6499d54635a3b10c28613f03ce7
Sean Stephens <D00211442@student.dkit.ie>
2019-11-05 12:54:50 +0000

    Update for home

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', fetchProducts)
+
+
+function fetchProducts() {
+
+    fetch('db-connect.php')
+        .then(res => res.json())
+        .then(json => {
+            document
+                .querySelector('#productsContainer')
+                .innerHTML = json.map(product => '
+                <li>${product.name}: ${product.price}</li>
+                ').join('')
+        
+        });
+}
