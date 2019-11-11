2b1516a32d8b13c1cd7a091a950a3773dc6d9d1a
Sean Stephens <D00211442@student.dkit.ie>
2019-11-11 10:55:25 +0000

    ddd

--- db-connect.php
+++ db-connect.php
@@ -1,18 +1,19 @@
 <?php
 
 $db_name = 'sean_products';
-$db_user = 'sean';
-$db_pass = '17millroad';
+$db_user = 'ss17';
+$db_pass = 'YES';
+try{
+$db      = new PDO("mysql:host=localhost;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
 
-$db      = new PDO("mysql:host=localhost;dbname=$db_name;
-charset=utf8mb4", $db_user, $db_pass);
-
-$statement = $db->prepare('SELECT id,name,price,description from products');
+$statement = $db->prepare('SELECT ProductID,Name,Price,Description from products');
 $statement->execute();
 
 $products = $statement->fetchAll();
 
 header('Content-Type: application/json');
 echo json_encode($products);
-
+} catch (Exception $e) {
+    echo 'PDO Error: ',  $e->getMessage(), "\n";
+}
 ?>
